03a0f3019da0cc2d0d3db88dbbf4ffbab4bf31f9
vitaly_sereda <tifoha@gmail.com>
2015-09-29 08:48:47 +0300

    jhg

--- Ch04_GenericsAndCollections/src/main/java/streams/ch05/Examples.java
+++ Ch04_GenericsAndCollections/src/main/java/streams/ch05/Examples.java
@@ -1,23 +1,38 @@
 package streams.ch05;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.Test;
+import streams.entities.Album;
+import streams.entities.Artist;
 
 import java.util.*;
+import java.util.function.*;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.*;
 import static org.junit.Assert.*;
+import static streams.utils.TestData.getAlbumStream;
+import static streams.utils.TestData.getAlbums;
 
 /**
  * Created by vitaly on 24.09.15.
  */
 
 public class Examples {
+    public List<Integer> numbersUnsorted = Arrays.asList(4, 5, 1, 3, 2, 6);
+    public List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+//    public static void main(String[] args) {
+//        String artistNames = getAlbums().get(0).getMainMusician().getMembers().map(Artist::getName).collect(stringCollector(":", "(", ")"));
+//        System.out.println(artistNames);
+//    }
     @Test
     public void main() {
-        List<Integer> numbersUnsorted = Arrays.asList(4, 5, 1, 3, 2, 6);
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
         List<Integer> sameOrder = numbers.stream().collect(toList());
         List<Integer> hashOrder = new HashSet<>(numbersUnsorted).stream().collect(toList());
         List<Integer> sorted = numbersUnsorted.stream().sorted().collect(toList());
@@ -29,5 +44,98 @@ public class Examples {
         List<Integer> integerList = IntStream.range(0, numbersUnsorted.size()).mapToObj(numbersUnsorted::get).collect(toList());
         System.out.println(integerList);
 
+
+    }
+
+    @Test
+    public void collectors() {
+//        System.out.println(getAlbums());
+        HashSet<Integer> intSet = numbersUnsorted.stream().collect(toCollection(HashSet::new));
+        assertEquals(intSet, new HashSet<Integer>(numbersUnsorted));
+
+        Album lastAlbum = getAlbumStream().collect(maxBy(comparingInt(Album::getYear))).get();
+        assertEquals(getAlbums().get(6), lastAlbum);
+
+        Map<Boolean, List<Album>> albumMap = getAlbumStream()
+                .collect(partitioningBy(album -> album.getMainMusician().isSolo()));
+        System.out.println(albumMap);
+        System.out.println();
+
+        Map<Integer, List<Album>> albumsByYears = getAlbumStream()
+                .collect(groupingBy(Album::getYear));
+        System.out.println(albumsByYears);
+        System.out.println();
+
+        String artists = getAlbums().get(0).getMainMusician().getMembers().map(Artist::getName).collect(joining(", ", "{", "}"));
+        System.out.println(artists);
+
+        Map<Integer, Long> albumsByYearsCount = getAlbumStream()
+                .collect(groupingBy(Album::getYear, counting()));
+        System.out.println(albumsByYearsCount);
+        System.out.println();
+
+        Map<Integer, List<String>> albums = getAlbumStream().collect(groupingBy(Album::getYear, mapping(Album::getName, toCollection(LinkedList::new))));
+        System.out.println(albums);
+        System.out.println();
+
+        String artistNames = getAlbums().get(0).getMainMusician().getMembers().map(Artist::getName).collect(stringCollector(":", "(", ")"));
+        System.out.println(artistNames);
+
+
+    }
+
+    private static class Combiner implements BinaryOperator<StringBuilder> {
+
+        @Override
+        public StringBuilder apply(StringBuilder sb1, StringBuilder sb2) {
+            int i = 0 + 7;
+            return sb1.append(sb2);
+        }
+    }
+    public static Collector<String, StringBuilder, String> stringCollector(String delimiter, String prefix, String suffix) {
+        return new Collector<String, StringBuilder, String>() {
+            @Override
+            public Supplier<StringBuilder> supplier() {
+                return StringBuilder::new;
+            }
+
+            @Override
+            public BiConsumer<StringBuilder, String> accumulator() {
+
+                return (sb, s) -> {
+                    if (sb.length() > 1) {
+                        sb.append(delimiter);
+                    }
+                    sb.append(s);
+
+                };
+            }
+
+            @Override
+            public BinaryOperator<StringBuilder> combiner() {
+                return null;
+//                return (stringBuilder, stringBuilder2) -> stringBuilder;
+//                return new Combiner();
+//                return (sb1, sb2) -> {
+//                    int i = 0;
+//                    return sb1.append(sb2);
+//                };
+            }
+            //Бренди Виски, Гектор, Гелий, Изюм, Зевс, Изя, Крис, Крон, Круз, Марс, Барт, Бакс, Оскар, Гарфилд, Феликс, Рубин, Бус
+
+            @Override
+            public Function<StringBuilder, String> finisher() {
+                return sb -> {
+                    sb.insert(0, prefix);
+                    sb.append(suffix);
+                    return sb.toString();
+                };
+            }
+
+            @Override
+            public Set<Characteristics> characteristics() {
+                return Collections.emptySet();
+            }
+        };
     }
 }
--- Ch04_GenericsAndCollections/src/main/java/streams/ch05/Exercises.java
+++ Ch04_GenericsAndCollections/src/main/java/streams/ch05/Exercises.java
@@ -1,5 +1,14 @@
 package streams.ch05;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by vitaly on 22.09.15.
  */
@@ -7,7 +16,21 @@ public class Exercises {
     public static void main(String[] args) {
 //        1
 //        2
+        List<String> names = Arrays.asList("John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr", "Pete Best", "Stuart Sutcliffe");
+        String longest = names.stream().reduce((s, s2) -> s2.length() > s.length() ? s2 : s).get();
+        System.out.println(longest);
+
+        longest = names.stream().collect(Collectors.maxBy(Comparator.comparing(String::length))).get();
+        System.out.println(longest);
+
+        Stream<String> names2 = Stream.of("John", "Paul", "George", "John", "Paul", "John");
+        System.out.println(names2.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
+//todo реализовать коллектор
+//        Collector<String, Map, Map> g = new Collector()
 //        3
+        for (int i = 0; i < 5; i++) {
+
+        }
 //        4
 //        5
 //        6
